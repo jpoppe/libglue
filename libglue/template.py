f4006b4a054c722b14c4aa77c9f01f3d7044938e
Jasper Poppe <jgpoppe@gmail.com>
2023-01-15 16:38:57 +0100

    Add more typings and string template render function 🖌️

--- libglue/template.py
+++ libglue/template.py
@@ -5,24 +5,27 @@ __copyright__ = "Copyright 2012-2022 Jasper Poppe"
 __license__ = "MIT"
 __status__ = "Development"
 
+import os
+import string
 from pathlib import Path
+from typing import Any
 
 import jinja2
 
 from .console import log
-from .file_system import read_file
+from .file_system import read_file, write_file
 
 
 class Template:
     """Render Jinja2 template."""
 
-    def __init__(self, template_path):
+    def __init__(self, template_path: Path):
         """Initialize class variables."""
         self.jinja_env = jinja2.Environment(
             extensions=["jinja2.ext.do", "jinja2.ext.loopcontrols"], loader=jinja2.FileSystemLoader(template_path)
         )
 
-    def render(self, file_name, data):
+    def render(self, file_name: Path, data: Any):
         """Render jinja template."""
         template = self.jinja_env.from_string(read_file(file_name))
 
@@ -33,11 +36,21 @@ class Template:
             log.error(msg, file_name, data, error)
             return None
 
-    def write(self, src_file: Path, dst_file: Path, data):
+    def write(self, source: Path, destination: Path, data: Any):
         """Render and write Jinja2 template."""
-        content = self.render(src_file, data)
+        content = self.render(source, data)
+
         if not content:
             return False
 
-        dst_file.expanduser().write_text(content)
+        destination.expanduser().write_text(content)
         return True
+
+
+def render_template(source: Path, destination: Path, template_vars: Any, chmod: int | None = None):
+    """Render template with string Template class."""
+    template = string.Template(read_file(source))
+    write_file(destination, template.substitute(template_vars))
+
+    if chmod is not None:
+        os.chmod(destination, int(str(chmod), 8))
