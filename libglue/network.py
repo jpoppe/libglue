0e565656456ef2376baa1b4dd1a5a75bb8dcaa57
Jasper Poppe <jgpoppe@gmail.com>
2023-01-15 16:41:47 +0100

    Use `shell` instead of `run_command` 🐚

--- libglue/network.py
+++ libglue/network.py
@@ -1,23 +1,23 @@
 """libGlue network library."""
 
-import xmltodict
-
-from .shell import run_command
-
 __author__ = "Jasper Poppe"
 __copyright__ = "Copyright 2012-2022 Jasper Poppe"
 __license__ = "MIT"
 __status__ = "Development"
 
+import xmltodict
+
+from .shell import shell
+
 
 def ip_scan(ip_range: str):
     """Scan IP addresses and return dictionary."""
-    xml_response = run_command(f"sudo nmap -sP {ip_range} -oX -").stdout.decode()
+    xml_response = shell("sudo", "nmap", "-sP", ip_range, "-oX", "-")
 
     result = []
-    for host in xmltodict.parse(xml_response, dict_constructor=dict, force_list=("address",), attr_prefix="")[
-        "nmaprun"
-    ]["host"]:
+    for host in xmltodict.parse(
+        xml_response, dict_constructor=dict, force_list=("address",), attr_prefix=""
+    )["nmaprun"]["host"]:
         item = {"state": host["status"]["state"]}
 
         if "hostnames" in host and host["hostnames"]:
